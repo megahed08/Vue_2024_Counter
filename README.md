91571bc9fcdbdc344c64cf4ee1155d67fddacc9f
Youssef Saleh <youssef.saleh@cloudrizon.com>
2024-03-25 14:30:27 +0100

    Project start

---
+++ README.md
@@ -0,0 +1,16 @@
+A simple vue js project of a counter with an input and two buttons to increment and decrement 
+
+# Vite Vue Starter
+
+This is a project template using [Vite](https://vitejs.dev/). It requires [Node.js](https://nodejs.org) version 18+, 20+.
+
+To start:
+
+```sh
+npm install
+npm run dev
+
+# if using yarn:
+yarn
+yarn dev
+```
---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  data() {
+    return {
+      msg: 'Counter',
+      count: 0
+    }
+  },
+  methods:{
+    inc() {this.count = parseInt(String(this.count)) + 1;},
+    dec() {this.count -= 1;}
+  }
+}
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <input v-model="count"> 
+  <h1>{{count}}</h1>
+  <button @click="inc">+</button>
+  <button @click="dec">-</button>
+</template>
+
+<style>
+
+</style>
